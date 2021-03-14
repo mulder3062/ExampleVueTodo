d05cc6e6ba9ab59b67409b162ebbbf9836ddc08c
mulder3062@gmail.com <gi123qweasd>
2021-03-14 14:45:44 +0900

    Step4 - 자식 컴포넌트 Style

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <span v-if="!item.checked">
+  <span class="progress" v-if="!item.checked">
       <input type="checkbox" v-on:change="checkItem(item)">
       <input class="todo-title"
              type="text"
@@ -8,7 +8,7 @@
       />
      <button v-on:click="deleteItem(item.id)">삭제</button>
   </span>
-  <span v-else>
+  <span class="done" v-else>
     <input type="checkbox" v-on:change="checkItem(item)" checked>
     <span>{{ item.title ? item.title : '(내용 없음)' }}</span>
     <button v-on:click="deleteItem(item.id)">삭제</button>
@@ -33,3 +33,18 @@ export default {
   }
 }
 </script>
+
+<!-- scoped 라는 범위를 지정하면 이 컴포넌트와 이 컴포넌트의 자식에만 스타일이 적용되며 부모 컴포넌트에는 적용되지 않는다 -->
+<style scoped>
+button {
+  background-color: orange;
+}
+
+.progress input {
+  color: black
+}
+
+.done span {
+  text-decoration: line-through;
+}
+</style>
